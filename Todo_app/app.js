17ba5dd951fe8a3f2b916acb215bb18db4b6a883
DannyL <dannylopez8342@gmail.com>
2025-11-03 12:33:56 -0600

    Add files via upload

---
+++ Todo_app/app.js
@@ -0,0 +1,24 @@
+document.getElementById("btnAgregar").addEventListener("click", agregarTarea);
+
+function agregarTarea() {
+  const texto = document.getElementById("tarea").value;
+  if (texto == "") {
+    alert("Escribe algo por favor")
+    return
+  }
+  nuevaTarea = document.createElement("li");
+  nuevaTarea.textContent = texto;
+  nuevaTarea.innerHTML = texto + " <button onclick='eliminar(this)'>X</button>";
+  document.getElementById("listaTareas").appendChild(nuevaTarea);
+  document.getElementById("tarea").value = "";
+}
+
+function eliminar(btn) {
+  if (confirm("¿Seguro que deseas eliminar?") == true) {
+    btn.parentNode.remove();
+  }
+}
+
+function tareaInutil() {
+  console.log("Esto nunca se ejecuta");
+}
